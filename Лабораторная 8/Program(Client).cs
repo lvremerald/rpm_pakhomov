3c2058b61c13c47d00974cd4f993c899ec1cf1cd
lvremerald <139621749+lvremerald@users.noreply.github.com>
2026-04-02 11:08:14 +0500

    Add files via upload

---
+++ Лабораторная 8/Program(Client).cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace NamedPipeStreamClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var s = new NamedPipeClientStream("Message_pipe"))
+            {
+                Console.WriteLine("Ожидание сервера...");
+                s.Connect();
+                Console.Clear();
+                StreamReader sr = new StreamReader(s);
+                StreamWriter sw = new StreamWriter(s);
+                Console.WriteLine("Напишите сообщение. Напишите \"СТОП\" для выхода.");
+                string msg = Console.ReadLine();
+                bool flag = false;
+                while (msg.ToLower() != "стоп")
+                {
+                    if (!flag)
+                    {
+                        sw.WriteLine(msg);
+                        sw.Flush();
+                        flag = true;
+                    }
+                    else
+                    {
+                        msg = sr.ReadLine();
+                        Console.WriteLine("> " + msg);
+                        Console.WriteLine("Напишите сообщение.");
+                        msg = Console.ReadLine();
+                        flag = false;
+                    }
+                }
+                sw.WriteLine("СТОП");
+                sw.Flush();
+                sw.Close();
+                sr.Close();
+                s.Close();
+            }
+            Console.WriteLine("Сеанс клиента закончен");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Лабораторная 8/Program(Server).cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace NamedPipeStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var s = new NamedPipeServerStream("Message_pipe"))
+                {
+                    Console.WriteLine("Ожидание клиента...");
+                    s.WaitForConnection();
+                    Console.Clear();
+                    Console.WriteLine("Ожидание сообщения от клиента");
+                    StreamReader sr = new StreamReader(s);
+                    StreamWriter sw = new StreamWriter(s);
+                    string msg = sr.ReadLine();
+                    while (msg.ToLower() != "стоп")
+                    {
+                        Console.WriteLine("> " + msg);
+                        string buf = "";
+                        foreach (char c in msg)
+                        {
+                            buf += Convert.ToInt32(c).ToString() + " ";
+                        }
+                        msg = buf;
+                        sw.WriteLine(msg);
+                        sw.Flush();
+                        Console.WriteLine(msg);
+                        Console.WriteLine("Ожидание сообщения от клиента");
+                        msg = sr.ReadLine();
+                    }
+                    sw.Close();
+                    sr.Close();
+                    s.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Сеанс сервера закончен");
+            Console.ReadLine();
+        }
+    }
+}
